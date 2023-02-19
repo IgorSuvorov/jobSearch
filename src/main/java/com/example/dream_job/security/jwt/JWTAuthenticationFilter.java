76d3e514d03681164a3939b6292c440b6c08939e
isuvorov1 <isuvorov1@outlook.com>
2023-02-19 17:07:16 -0500

    keep going

--- src/main/java/com/example/dream_job/security/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/example/dream_job/security/jwt/JWTAuthenticationFilter.java
@@ -68,7 +68,7 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
         String bearerToken = request.getHeader("Authorization");
 
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7, bearerToken.length());
         }
 
