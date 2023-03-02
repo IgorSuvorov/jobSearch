5f0d0b62327129aea0fe40880d80ef67f6a0f2b7
isuvorov1 <isuvorov1@outlook.com>
2023-03-02 11:53:17 -0500

    keep going

--- src/main/java/com/example/dream_job/controller/AuthController.java
+++ src/main/java/com/example/dream_job/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     @Autowired
     public AuthController(AuthService authService) {
